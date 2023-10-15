5d869d4fa65e7c38ee5496c5afec6418b19d81f1
Jusang98 <rlatkdwn3379@gmail.com>
2023-10-15 17:53:56 +0900

    Initial commit

---
+++ src/main/java/com/study/Board/BoardProjectForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.study.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectForSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/Board/BoardProjectForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectForSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
